784a139980474160818f76a867bbc98d8279bb16
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-09-20 15:41:20 +0700

    Run prject with Docker

---
+++ 11-training-php/configs/database.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'web-mysql');   // phải trùng với tên service trong docker-compose
+define('DB_USER', 'user');        // DB_USERNAME trong .env
+define('DB_PASSWORD', 'pass');    // DB_PASSWORD trong .env
+define('DB_NAME', 'app_web1');    // DB_DATABASE trong .env
+define('DB_PORT', 3306);          // DB_PORT trong .env
